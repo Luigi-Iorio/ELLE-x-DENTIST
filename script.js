6f25d6061d490acb041abd0cca56a4afa8199517
Luigi <98226243+Luigi-Iorio@users.noreply.github.com>
2023-04-18 23:31:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function menu() {
+  let navBar = document.querySelector(".navBar");
+  navBar.classList.toggle("active");
+};
+
+function ask() {
+  let trow = document.querySelector(".tRow");
+  trow.classList.toggle("disattivo");
+};
+
+function ask1() {
+  let trow = document.querySelector(".uno");
+  trow.classList.toggle("attivo");
+};
+
+function ask2() {
+  let trow = document.querySelector(".due");
+  trow.classList.toggle("attivo");
+};
+
+function ask3() {
+  let trow = document.querySelector(".tre");
+  trow.classList.toggle("attivo");
+};
